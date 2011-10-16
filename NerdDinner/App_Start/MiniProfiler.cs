c6da51cadbd8b8127eb1f07157056d964ede3afd
Seth Webster <sethwebster@gmail.com>
2011-10-16 10:12:13 -0700

    Removed Mini Profiler for now since it seems to be causing issues

--- NerdDinner/App_Start/MiniProfiler.cs
+++ NerdDinner/App_Start/MiniProfiler.cs
@@ -26,38 +26,38 @@ namespace NerdDinner.App_Start
         public static void PreStart()
         {
 
-            // Be sure to restart you ASP.NET Developement server, this code will not run until you do that. 
+            //// Be sure to restart you ASP.NET Developement server, this code will not run until you do that. 
 
-            //TODO: See - _MINIPROFILER UPDATED Layout.cshtml
-            //      For profiling to display in the UI you will have to include the line @MvcMiniProfiler.MiniProfiler.RenderIncludes() 
-            //      in your master layout
+            ////TODO: See - _MINIPROFILER UPDATED Layout.cshtml
+            ////      For profiling to display in the UI you will have to include the line @MvcMiniProfiler.MiniProfiler.RenderIncludes() 
+            ////      in your master layout
 
-            //TODO: Non SQL Server based installs can use other formatters like: new MvcMiniProfiler.SqlFormatters.InlineFormatter()
-            MiniProfiler.Settings.SqlFormatter = new MvcMiniProfiler.SqlFormatters.SqlServerFormatter();
+            ////TODO: Non SQL Server based installs can use other formatters like: new MvcMiniProfiler.SqlFormatters.InlineFormatter()
+            //MiniProfiler.Settings.SqlFormatter = new MvcMiniProfiler.SqlFormatters.SqlServerFormatter();
 
-			//TODO: To profile a standard DbConnection: 
-			// var profiled = new ProfiledDbConnection(cnn, MiniProfiler.Current);
+            ////TODO: To profile a standard DbConnection: 
+            //// var profiled = new ProfiledDbConnection(cnn, MiniProfiler.Current);
 
-            //TODO: If you are profiling EF code first try: 
-			MiniProfilerEF.Initialize();
+            ////TODO: If you are profiling EF code first try: 
+            //MiniProfilerEF.Initialize();
 
-            //Make sure the MiniProfiler handles BeginRequest and EndRequest
-            DynamicModuleUtility.RegisterModule(typeof(MiniProfilerStartupModule));
+            ////Make sure the MiniProfiler handles BeginRequest and EndRequest
+            //DynamicModuleUtility.RegisterModule(typeof(MiniProfilerStartupModule));
 
-            //Setup profiler for Controllers via a Global ActionFilter
-            GlobalFilters.Filters.Add(new ProfilingActionFilter());
+            ////Setup profiler for Controllers via a Global ActionFilter
+            //GlobalFilters.Filters.Add(new ProfilingActionFilter());
         }
 
         public static void PostStart()
         {
-            // Intercept ViewEngines to profile all partial views and regular views.
-            // If you prefer to insert your profiling blocks manually you can comment this out
-            var copy = ViewEngines.Engines.ToList();
-            ViewEngines.Engines.Clear();
-            foreach (var item in copy)
-            {
-                ViewEngines.Engines.Add(new ProfilingViewEngine(item));
-            }
+            //// Intercept ViewEngines to profile all partial views and regular views.
+            //// If you prefer to insert your profiling blocks manually you can comment this out
+            //var copy = ViewEngines.Engines.ToList();
+            //ViewEngines.Engines.Clear();
+            //foreach (var item in copy)
+            //{
+            //    ViewEngines.Engines.Add(new ProfilingViewEngine(item));
+            //}
         }
     }
 
