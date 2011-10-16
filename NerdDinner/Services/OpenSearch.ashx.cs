fbabeb15a263730c3ed1790350ff93db03079f88
Seth Webster <sethwebster@gmail.com>
2011-10-16 09:43:14 -0700

    Updated text titles from NerdDinner to VegDinner

--- NerdDinner/Services/OpenSearch.ashx.cs
+++ NerdDinner/Services/OpenSearch.ashx.cs
@@ -18,7 +18,7 @@ namespace NerdDinner
             {
                 return new Description
                 {
-                    DisplayName = "NerdDinner.com",
+                    DisplayName = "VegDinner.co",
                     LongDescription = "Nerd Dinner - Organizing the world's nerds and helping them eat in packs",
                     SearchPathTemplate = "/Dinners?q={0}",
                     IconPath = "~/favicon.ico"
