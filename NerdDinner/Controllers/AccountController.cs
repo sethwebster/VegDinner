1493e8d31a71f4e0a1b9d62992482f031d211a6d
sethwebster <sethwebster@gmail.com>
2011-10-21 19:09:24 -0700

    Creation is working; date is still wonky

--- NerdDinner/Controllers/AccountController.cs
+++ NerdDinner/Controllers/AccountController.cs
@@ -10,37 +10,44 @@ using System.Web.UI;
 using NerdDinner.Helpers;
 using NerdDinner.Models;
 
-namespace NerdDinner.Controllers {
+namespace NerdDinner.Controllers
+{
 
-	  [HandleErrorWithELMAH]
-    public class AccountController : Controller {
+    [HandleErrorWithELMAH]
+    public class AccountController : Controller
+    {
 
         // This constructor is used by the MVC framework to instantiate the controller using
         // the default forms authentication and membership providers.
 
         public AccountController()
-            : this(null, null) {
+            : this(null, null)
+        {
         }
 
         // This constructor is not used by the MVC framework but is instead provided for ease
         // of unit testing this type. See the comments at the end of this file for more
         // information.
-        public AccountController(IFormsAuthentication formsAuth, IMembershipService service) {
+        public AccountController(IFormsAuthentication formsAuth, IMembershipService service)
+        {
             FormsAuth = formsAuth ?? new FormsAuthenticationService();
             MembershipService = service ?? new AccountMembershipService();
         }
 
-        public IFormsAuthentication FormsAuth {
+        public IFormsAuthentication FormsAuth
+        {
             get;
             private set;
         }
 
-        public IMembershipService MembershipService {
+        public IMembershipService MembershipService
+        {
             get;
             private set;
         }
 
-        public ActionResult LogOn() {
+        public ActionResult LogOn()
+        {
 
             return View();
         }
@@ -79,14 +86,16 @@ namespace NerdDinner.Controllers {
             return View(model);
         }
 
-        public ActionResult LogOff() {
+        public ActionResult LogOff()
+        {
 
             FormsAuth.SignOut();
 
             return RedirectToAction("Index", "Home");
         }
 
-        public ActionResult Register() {
+        public ActionResult Register()
+        {
 
             ViewBag.PasswordLength = MembershipService.MinPasswordLength;
 
@@ -120,7 +129,8 @@ namespace NerdDinner.Controllers {
         }
 
         [Authorize]
-        public ActionResult ChangePassword() {
+        public ActionResult ChangePassword()
+        {
 
             ViewBag.PasswordLength = MembershipService.MinPasswordLength;
 
@@ -131,98 +141,121 @@ namespace NerdDinner.Controllers {
         [HttpPost]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes",
             Justification = "Exceptions result in password not being changed.")]
-        public ActionResult ChangePassword(string currentPassword, string newPassword, string confirmPassword) {
+        public ActionResult ChangePassword(string currentPassword, string newPassword, string confirmPassword)
+        {
 
             ViewBag.PasswordLength = MembershipService.MinPasswordLength;
 
-            if (!ValidateChangePassword(currentPassword, newPassword, confirmPassword)) {
+            if (!ValidateChangePassword(currentPassword, newPassword, confirmPassword))
+            {
                 return View();
             }
 
-            try {
-                if (MembershipService.ChangePassword(User.Identity.Name, currentPassword, newPassword)) {
+            try
+            {
+                if (MembershipService.ChangePassword(User.Identity.Name, currentPassword, newPassword))
+                {
                     return RedirectToAction("ChangePasswordSuccess");
                 }
-                else {
+                else
+                {
                     ModelState.AddModelError("_FORM", "The current password is incorrect or the new password is invalid.");
                     return View();
                 }
             }
-            catch {
+            catch
+            {
                 ModelState.AddModelError("_FORM", "The current password is incorrect or the new password is invalid.");
                 return View();
             }
         }
 
-        public ActionResult ChangePasswordSuccess() {
+        public ActionResult ChangePasswordSuccess()
+        {
 
             return View();
         }
 
         protected override void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            if (filterContext.HttpContext.User.Identity is WindowsIdentity) {
+            if (filterContext.HttpContext.User.Identity is WindowsIdentity)
+            {
                 throw new InvalidOperationException("Windows authentication is not supported.");
             }
         }
 
         #region Validation Methods
 
-        private bool ValidateChangePassword(string currentPassword, string newPassword, string confirmPassword) {
-            if (String.IsNullOrEmpty(currentPassword)) {
+        private bool ValidateChangePassword(string currentPassword, string newPassword, string confirmPassword)
+        {
+            if (String.IsNullOrEmpty(currentPassword))
+            {
                 ModelState.AddModelError("currentPassword", "You must specify a current password.");
             }
-            if (newPassword == null || newPassword.Length < MembershipService.MinPasswordLength) {
+            if (newPassword == null || newPassword.Length < MembershipService.MinPasswordLength)
+            {
                 ModelState.AddModelError("newPassword",
                     String.Format(CultureInfo.CurrentCulture,
                          "You must specify a new password of {0} or more characters.",
                          MembershipService.MinPasswordLength));
             }
 
-            if (!String.Equals(newPassword, confirmPassword, StringComparison.Ordinal)) {
+            if (!String.Equals(newPassword, confirmPassword, StringComparison.Ordinal))
+            {
                 ModelState.AddModelError("_FORM", "The new password and confirmation password do not match.");
             }
 
             return ModelState.IsValid;
         }
 
-        private bool ValidateLogOn(string userName, string password) {
-            if (String.IsNullOrEmpty(userName)) {
+        private bool ValidateLogOn(string userName, string password)
+        {
+            if (String.IsNullOrEmpty(userName))
+            {
                 ModelState.AddModelError("username", "You must specify a username.");
             }
-            if (String.IsNullOrEmpty(password)) {
+            if (String.IsNullOrEmpty(password))
+            {
                 ModelState.AddModelError("password", "You must specify a password.");
             }
-            if (!MembershipService.ValidateUser(userName, password)) {
+            if (!MembershipService.ValidateUser(userName, password))
+            {
                 ModelState.AddModelError("_FORM", "The username or password provided is incorrect.");
             }
 
             return ModelState.IsValid;
         }
 
-        private bool ValidateRegistration(string userName, string email, string password, string confirmPassword) {
-            if (String.IsNullOrEmpty(userName)) {
+        private bool ValidateRegistration(string userName, string email, string password, string confirmPassword)
+        {
+            if (String.IsNullOrEmpty(userName))
+            {
                 ModelState.AddModelError("username", "You must specify a username.");
             }
-            if (String.IsNullOrEmpty(email)) {
+            if (String.IsNullOrEmpty(email))
+            {
                 ModelState.AddModelError("email", "You must specify an email address.");
             }
-            if (password == null || password.Length < MembershipService.MinPasswordLength) {
+            if (password == null || password.Length < MembershipService.MinPasswordLength)
+            {
                 ModelState.AddModelError("password",
                     String.Format(CultureInfo.CurrentCulture,
                          "You must specify a password of {0} or more characters.",
                          MembershipService.MinPasswordLength));
             }
-            if (!String.Equals(password, confirmPassword, StringComparison.Ordinal)) {
+            if (!String.Equals(password, confirmPassword, StringComparison.Ordinal))
+            {
                 ModelState.AddModelError("_FORM", "The new password and confirmation password do not match.");
             }
             return ModelState.IsValid;
         }
 
-        private static string ErrorCodeToString(MembershipCreateStatus createStatus) {
+        private static string ErrorCodeToString(MembershipCreateStatus createStatus)
+        {
             // See http://msdn.microsoft.com/en-us/library/system.web.security.membershipcreatestatus.aspx for
             // a full list of status codes.
-            switch (createStatus) {
+            switch (createStatus)
+            {
                 case MembershipCreateStatus.DuplicateUserName:
                     return "Username already exists. Please enter a different user name.";
 
@@ -262,13 +295,16 @@ namespace NerdDinner.Controllers {
     // how to create an abstract wrapper around such a type in order to make the AccountController
     // code unit testable.
 
-    public interface IFormsAuthentication {
+    public interface IFormsAuthentication
+    {
         void SignIn(string userName, bool createPersistentCookie);
         void SignOut();
     }
 
-    public class FormsAuthenticationService : IFormsAuthentication {
-        public void SignIn(string userName, bool createPersistentCookie) {
+    public class FormsAuthenticationService : IFormsAuthentication
+    {
+        public void SignIn(string userName, bool createPersistentCookie)
+        {
             FormsAuthenticationTicket authTicket = new FormsAuthenticationTicket(
                 1, //version
                 userName, // user name
@@ -280,12 +316,14 @@ namespace NerdDinner.Controllers {
             string encTicket = FormsAuthentication.Encrypt(authTicket);
             HttpContext.Current.Response.Cookies.Add(new HttpCookie(FormsAuthentication.FormsCookieName, encTicket));
         }
-        public void SignOut() {
+        public void SignOut()
+        {
             FormsAuthentication.SignOut();
         }
     }
 
-    public interface IMembershipService {
+    public interface IMembershipService
+    {
         int MinPasswordLength { get; }
 
         bool ValidateUser(string userName, string password);
@@ -294,45 +332,59 @@ namespace NerdDinner.Controllers {
         bool ChangePassword(string userName, string oldPassword, string newPassword);
     }
 
-    public class AccountMembershipService : IMembershipService {
+    public class AccountMembershipService : IMembershipService
+    {
         private MembershipProvider _provider;
 
         public AccountMembershipService()
-            : this(null) {
+            : this(null)
+        {
         }
 
-        public AccountMembershipService(MembershipProvider provider) {
+        public AccountMembershipService(MembershipProvider provider)
+        {
             _provider = provider ?? Membership.Provider;
         }
 
-        public int MinPasswordLength {
-            get {
+        public int MinPasswordLength
+        {
+            get
+            {
                 return _provider.MinRequiredPasswordLength;
             }
         }
 
-        public bool ValidateUser(string userName, string password) {
+        public bool ValidateUser(string userName, string password)
+        {
+#if OFFLINE
+            return true;
+#endif
             return _provider.ValidateUser(userName, password);
         }
 
         public string GetCanonicalUsername(string userName)
         {
+#if OFFLINE
+            return "testinguser";
+#endif
             var user = _provider.GetUser(userName, true);
             if (user != null)
             {
-               return user.UserName;
+                return user.UserName;
             }
 
             return null;
         }
 
-        public MembershipCreateStatus CreateUser(string userName, string password, string email) {
+        public MembershipCreateStatus CreateUser(string userName, string password, string email)
+        {
             MembershipCreateStatus status;
             _provider.CreateUser(userName, password, email, null, null, true, null, out status);
             return status;
         }
 
-        public bool ChangePassword(string userName, string oldPassword, string newPassword) {
+        public bool ChangePassword(string userName, string oldPassword, string newPassword)
+        {
             MembershipUser currentUser = _provider.GetUser(userName, true /* userIsOnline */);
             return currentUser.ChangePassword(oldPassword, newPassword);
         }
